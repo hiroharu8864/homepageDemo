c9ae1cdb6347f76f9619e05915fc24367b56e700
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-05-17 09:05:23 +0900

    add materialUI

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,8 +1,11 @@
+import PublicOffIcon from "@mui/icons-material/PublicOff";
+
 export const NotFound = () => {
   return (
     <div>
       <h1>404 NotFound</h1>
       <h3>お探しのページは見つかりませんでした。</h3>
+      <PublicOffIcon />
     </div>
   );
 };
