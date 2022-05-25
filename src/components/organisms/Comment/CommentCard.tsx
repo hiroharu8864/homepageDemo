11ef9a3030ef247a998ed1f835bbe4e2c3b56638
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-05-26 08:27:52 +0900

    CommendCardサンプル完成

--- src/components/organisms/Comment/CommentCard.tsx
+++ src/components/organisms/Comment/CommentCard.tsx
@@ -5,21 +5,22 @@ export const CommentCard = () => {
     background-color: #e4f9f5;
     box-shadow: #ddd 0px 0px 4px 2px;
     border-radius: 8px;
-    padding: 16px;
-    text-align: center;
+    padding: 1px;
     img {
       border-radius: 50%;
     }
     dl {
       margin-bottom: 0px;
       dt {
-        float: left;
+        text-align: left;
+        padding-left: 10px;
         font-weight: bold;
         margin: 0;
         color: #40514e;
       }
       dd {
-        padding-left: 32px;
+        padding-left: 10px;
+        margin: 0;
         padding-bottom: 4px;
         /* 折り返して表示 */
         overflow-wrap: break-word;
@@ -30,7 +31,7 @@ export const CommentCard = () => {
   return (
     <SDiv>
       <dl>
-        <dd>TEST</dd>
+        <dt>TESTaaaaa</dt>
         <dd>TEST</dd>
       </dl>
     </SDiv>
